0cc1d9d3dd407d537b099d1a6ed0c148446beeb6
max1m <maxim.koltunevich@succraft.com>
2019-10-23 16:47:25 +0300

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'inputProject';
+  title = 'Full Name';
+  value = '';
+  update(value: string) { this.value = value; }
+  valueName = '';
+  updateName(valueName: string) { this.valueName = valueName; }
 }
